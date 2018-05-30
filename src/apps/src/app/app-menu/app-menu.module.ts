721e8b54f6cf25fe9b1f457b6e7bd532f20f8b0e
Emanuel Pais <emanuel@econo.unlp.edu.ar>
2018-05-30 10:03:39 -0300

    Agregue material y lo probe con el module app-menu. QUedo funcionando

--- src/apps/src/app/app-menu/app-menu.module.ts
+++ src/apps/src/app/app-menu/app-menu.module.ts
@@ -1,10 +1,10 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { MenuComponent } from './menu/menu.component';
+import { MaterialModule } from '../material.module';
 
 @NgModule({
   imports: [
-    CommonModule
+    MaterialModule
   ],
   declarations: [
     MenuComponent
