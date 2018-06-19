588d4f27f8b8d7ac50862cf4f1dca999f5659af5
walterblanco <walter@creativehands.com.ar>
2018-06-19 15:04:23 -0300

    Arme un login nuevo para los sistemas

--- src/apps/src/app/app-routing.module.ts
+++ src/apps/src/app/app-routing.module.ts
@@ -3,9 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { RouterModule, Routes } from '@angular/router';
 import { PantallaPrincipalComponent } from './pantalla-principal/pantalla-principal.component';
+import { LoginComponent } from './login/login.component';
+
 
 const routes: Routes = [
 { path: 'inicial', component: PantallaPrincipalComponent },
+{ path: 'login', component: LoginComponent },
 { path: '', redirectTo: '/inicial', pathMatch: 'full' }
 
 ]
--- src/apps/src/app/app.module.ts
+++ src/apps/src/app/app.module.ts
@@ -6,11 +6,13 @@ import { AppRoutingModule } from './/app-routing.module';
 import { PantallaPrincipalComponent } from './pantalla-principal/pantalla-principal.component';
 import { AppMenuModule } from './app-menu/app-menu.module';
 import { MaterialModule } from './material.module';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PantallaPrincipalComponent
+    PantallaPrincipalComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
