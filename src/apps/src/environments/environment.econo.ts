6a0f3cd9a527c90b79b98cd3399c5b0499dfaa6b
Miguel Macagno <miguel.macagno@econo.unlp.edu.ar>
2018-08-24 08:42:46 -0300

    Se parametrizó el environment

--- src/apps/src/environments/environment.econo.ts
+++ src/apps/src/environments/environment.econo.ts
@@ -4,7 +4,7 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: true,
+  production: false,
   assistanceApiUrl: 'https://api.econo.unlp.edu.ar/assistance/api/v1.0',
   oidp_issuer: 'https://oidp.econo.unlp.edu.ar',
   userinfoEndpoint: 'https://oidp.econo.unlp.edu.ar/userinfo',
--- src/apps/src/environments/environment.ts
+++ src/apps/src/environments/environment.ts
@@ -1,15 +1,16 @@
-// This file can be replaced during build by using the `fileReplacements` array.
-// `ng build ---prod` replaces `environment.ts` with `environment.prod.ts`.
-// The list of file replacements can be found in `angular.json`.
+// The file contents for the current environment will overwrite these during build.
+// The build system defaults to the dev environment which uses `environment.ts`, but if you do
+// `ng build --env=prod` then `environment.prod.ts` will be used instead.
+// The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
-};
+  production: false,
+  assistanceApiUrl: 'https://api.econo.unlp.edu.ar/assistance/api/v1.0/',
+  oidp_issuer: 'https://oidp.econo.unlp.edu.ar',
+  userinfoEndpoint: 'https://oidp.econo.unlp.edu.ar/userinfo',
+  loginUrl: 'https://oidp.econo.unlp.edu.ar/oauth2/auth',
+  logoutUrl: 'https://consent.econo.unlp.edu.ar/logout'
+  // tokenEndpoint: 'https://oidp.econo.unlp.edu.ar/oauth2/auth'
 
-/*
- * In development mode, to ignore zone related error stack frames such as
- * `zone.run`, `zoneDelegate.invokeTask` for easier debugging, you can
- * import the following file, but please comment it out in production mode
- * because it will have performance impact when throw error
- */
-// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
+
+};
