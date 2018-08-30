2c23fe9a91bdcdc749f28abf120aceb9d74aca6a
Miguel Macagno <miguel.macagno@econo.unlp.edu.ar>
2018-08-30 09:06:43 -0300

    Se corrigieron carpetas de logos y se parametrizaron en environment

--- src/apps/src/environments/environment.presi.ts
+++ src/apps/src/environments/environment.presi.ts
@@ -13,14 +13,14 @@ export const environment = {
   loaderHeader: 'Aplicaciones | Presidencia',
   loaderTitle: 'Aplicaciones',
   loaderSubTitle: 'Sistema de Acceso Único del sistema de Asistencia | Presidencia UNLP.',
-  loaderLogo: '/assets/img/logo_fce.png',
+  loaderLogo: '/assets/img/presi/logo.png',
   loaderInfo1Title: 'Acceso Único',
   loaderInfo1Text: 'Unificación del acceso a los sistemas informáticos bajo un único usuario y contraseña.',
   loaderInfo2Title: 'Aplicaciones',
   loaderInfo2Text: 'Fácil y rápido acceso a los sistemas informáticos de la oficina de Personal - Presidencia UNLP.',
   loaderInfo3Title: 'Accesos y Permisos',
   loaderInfo3Text: 'Gestión de roles y permisos para el acceso a las aplicaciones.',
-  loaderInfo4Title: '/assets/img/ditesi.png',
+  loaderInfo4Title: '/assets/img/presi/logo.png',
   loaderInfo4Text: 'Dirección General de Personal | Presidencia - UNLP ',
   loaderFooter1: 'Dirección General de Personal | Presidencia | UNLP | Calle 7 Nº 776',
   loaderFooter2: 'Dirección General de Personal | Presidencia ',
@@ -32,7 +32,7 @@ export const environment = {
   footerEMail: 'miguel.macagno@presi.unlp.edu.ar',
   footerTelephone: '+54 (0221) 6447087 Interno 5809',
   footerAddress: 'Calle 7 Nº 776',
-  footerLogo: '/assets/img/logo_fce.png'
+  footerLogo: '/assets/img/presi/logo.png'
   // tokenEndpoint: 'https://oidp.econo.unlp.edu.ar/oauth2/auth'
 
 
--- src/apps/src/environments/environment.ts
+++ src/apps/src/environments/environment.ts
@@ -13,14 +13,14 @@ export const environment = {
   loaderHeader: 'Aplicaciones | FCE',
   loaderTitle: 'Aplicaciones FCE',
   loaderSubTitle: 'Sistema de Acceso Único de la  Facultad de Ciencias Económicas | UNLP.',
-  loaderLogo: '/assets/img/logo_fce.png',
+  loaderLogo: '/assets/img/econo/logo.png',
   loaderInfo1Title: 'Acceso Único',
   loaderInfo1Text: 'Unificación del acceso a los sistemas informáticos bajo un único usuario y contraseña.',
   loaderInfo2Title: 'Aplicaciones',
   loaderInfo2Text: 'Fácil y rápido acceso a los sistemas informáticos de la Facultad de Ciencias Económicas - UNLP.',
   loaderInfo3Title: 'Accesos y Permisos',
   loaderInfo3Text: 'Gestión de roles y permisos para el acceso a las aplicaciones.',
-  loaderInfo4Title: '/assets/img/ditesi.png',
+  loaderInfo4Title: '/assets/img/econo/ditesi.png',
   loaderInfo4Text: 'Dirección de tecnologías y Sistemas Informáticos | FCE - UNLP ',
   loaderFooter1: 'DiTESI | Dirección de tecnologías y Sistemas Informáticos | Facultad de Ciencias Económicas | UNLP | Calle 6 Nº 777',
   loaderFooter2: 'DiTESI | Dirección de tecnologías y Sistemas Informáticos | FCE ',
@@ -32,7 +32,7 @@ export const environment = {
   footerEMail: 'ditesi@econo.unlp.edu.ar',
   footerTelephone: '+54 (0221) 4236769 Interno 123',
   footerAddress: 'Calle 6 Nº 777 | 1º subsuelo',
-  footerLogo: '/assets/img/logo_fce.png'
+  footerLogo: '/assets/img/econo/logo.png'
   // tokenEndpoint: 'https://oidp.econo.unlp.edu.ar/oauth2/auth'
 
 
